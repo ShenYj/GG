5ce3dc5fbccfe4f9d7e83316a71a7286bc0fa9c2
Ryan Shen <shenyj4@51nb.com>
2018-09-25 17:34:37 +0800

    args使用

--- HelloWorld/helloworld2.go
+++ HelloWorld/helloworld2.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
-
 	const a, b = 10, 11
 	fmt.Println("a = ", a, " b = ", b)
-	//	fmt.Printf("a = %d", "b = %d", a, b)
-	//fmt.Println("Hello World!")
+	test(a)
+	test2(b, 20, 20, 20, 40, 60, 8)
+}
+func test(a int) {
+	fmt.Println(a)
+}
+
+// 多个不确定参数使用args， 必须放在最后
+func test2(a int, args ...int) {
+	fmt.Println(a)
+	for i := 0; i < len(args); i++ {
+		fmt.Println(args[i])
+	}
 }
