5b9007ebbd3646fc08df9241473ee1d1ec714810
ShenYj <shenyj@51nb.com>
2018-09-26 15:02:00 +0800

    不定参数，部分传递

--- 函数/不定参数列表函数.go
+++ 函数/不定参数列表函数.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	test2(1231, 3434, 5984)
+	test2(1231, 3434, 5984, 34, 556, 43)
 }
 
 func test1(a ...int) {
@@ -14,5 +14,6 @@ func test1(a ...int) {
 
 func test2(args ...int) {
 	fmt.Print("test2 调用Test1")
-	test1(args...)
+	//test1(args...) // 全部传给test1
+	test1(args[2:]...) // 从args[2]始传递， 包括args[2]自身
 }
