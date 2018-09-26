3c5153af432a1b7ec6d5cb24b6610ba96d032879
ShenYj <shenyj@51nb.com>
2018-09-26 17:15:38 +0800

    多个返回值

--- 函数/有返回值函数.go
+++ 函数/有返回值函数.go
@@ -5,8 +5,31 @@ import "fmt"
 func main() {
 	a := myFunc()
 	fmt.Println(a)
+
+	b, c, d := myFunc3()
+	fmt.Print(b)
+	fmt.Print(c)
+	fmt.Print(d)
 }
 
+// 有一个返回值函数
 func myFunc() int {
 	return 10
+}
+
+// 有一个返回值， 给返回值起一个变量名（GO语言推荐写法）
+func myFunc1() (result int) {
+	return 10
+}
+
+// 有一个返回值， 给返回值起一个变量名（GO语言推荐写法）
+func myFunc2() (result int) {
+	result = 10 // 常用写法
+	return
+}
+
+// 有多个返回值
+func myFunc3() (a, b, c int) {
+	a, b, c = 111, 222, 333
+	return
 }
